6e99142ed9d336bd9d97d02eeef3368e4dad1854
jeremy <102297631+proprophet2503@users.noreply.github.com>
2024-10-21 17:28:17 +0700

    Update nabilcrocs.py

--- nabilcrocs.py
+++ nabilcrocs.py
@@ -3,8 +3,11 @@ B = list(map(int, input().split()))
 
 hasil = 1
 for i in range(N):
-    for j in range(i+1, N):
-        perkalian_xor = B[i]^B[j]
-        hasil*= perkalian_xor
-        
+    for j in range(1+i, N):
+        if(B[i] != B[j]):
+            hasil *= B[i]^B[j]
+        else:
+            hasil = 0
+            break
 print(hasil)
+
